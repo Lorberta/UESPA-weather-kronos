0502568cd6bc1756bbc30384b6ad238cda2af973
Lorberta <johanna.skuhra@gmail.com>
2018-12-01 15:01:11 +0100

    added cities

--- server/routes/cities.js
+++ server/routes/cities.js
@@ -5,20 +5,83 @@ const router = express.Router();
 const axios = require('axios')
 
 const cities = {
-  "Kronos1": {
+  "bar-or-lhess": { //northern hemisphere
     // _id: 'Tssl',
     img: '',
-    name: "Ko'nos",
+    name: "Bar'or Lhess",
     // slug: 'kronos',
     owid: 6453366
   },
-  'Kronos2': {
-    // _id: 'Tssl2',
-    img: '',
-    name: "Ko'nos2",
-    // slug: 'kronos2',
+  "first-city": { //southern hemisphere
+    img: '.../images/firstcity',
+    name: "First City",
     owid: 6453366
-  }
+  },
+  "ha-r-kling-jaa": { //northern hemisphere
+    img: '',
+    name: "Ha'r Kling Jaa",
+    owid: 6453366
+  },
+  "h-vek-tar": { //southern hemisphere
+    img: '',
+    name: "H'Vek Tar",
+    owid: 6453366
+  },
+  "kl-alath": { //southern hemisphere
+    img: '',
+    name: "Kl'alath",
+    owid: 6453366
+  },
+  "l-chorta": { //northern hemisphere
+    img: '',
+    name: "L'chorta",
+    owid: 6453366
+  },
+  "lhess-taal-dax": { //northern hemisphere
+    img: '',
+    name: "Lhess Taal Dax",
+    owid: 6453366
+  },
+  "l-vln-lak-raal": { //northern hemisphere
+    img: '',
+    name: "L'vln Lak Raal",
+    owid: 6453366
+  },
+  "lyn-knor-tak": { //southern hemisphere
+    img: '',
+    name: "Lyn'Knor Tak",
+    owid: 6453366
+  },
+  "mor-em-cha": { //southern hemisphere
+    img: '',
+    name: "Mor'em Cha",
+    owid: 6453366
+  },
+  // "qam-chee": { //ancient city
+  //   img: '',
+  //   name: "Qam-Chee",
+  //   owid: 6453366
+  // },
+  // "quin-lat": { //ancient city
+  //   img: '',
+  //   name: "Quin'lat",
+  //   owid: 6453366
+  // },
+  // "tong-vey": { //ancient city
+  //   img: '',
+  //   name: "Tong Vey",
+  //   owid: 6453366
+  // },
+  "t-val-k-rang": { //southern hemisphere
+    img: '',
+    name: "T'Val K'rang",
+    owid: 6453366
+  },
+  "v-hechess": { //southern hemisphere
+    img: '',
+    name: "V'hechess",
+    owid: 6453366
+  },
 };
 
 router.get('/', (req, res, next) => {
