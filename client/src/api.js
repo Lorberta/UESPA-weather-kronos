cfd7a948304b23b6a002dbfd49559198d05a8834
Lorberta <johanna.skuhra@gmail.com>
2018-12-06 15:15:13 +0100

    Fix for Heroku

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+
 const service = axios.create({
   baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api',
   // "withCredentials: true" sends cookies
